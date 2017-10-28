cdb3eb730add25f797400d9e94d2a2d39f45d7a5
Harith Javed Bakhrani <harithjaved@gmail.com>
2017-10-28 11:37:09 +0000

    config file altered

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ class Config(object):
     Common configurations that are common across all environments
     '''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'development mode'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://testuser:abc123@localhost:5432/testdb'
 
 class DevelopmentConfig(Config):
     '''
