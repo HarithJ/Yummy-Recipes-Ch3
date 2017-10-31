f3a226a03507ec783d2aa33ef79602304d3b7a12
Harith Javed Bakhrani <harithjaved@gmail.com>
2017-10-31 20:27:31 +0000

    Setuped for heroku

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config(object):
     Common configurations that are common across all environments
     '''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #SECRET_KEY = 'development mode'
-    #SQLALCHEMY_DATABASE_URI = 'postgres://hlugpiywpgwxnx:cb0b209e0de7b05ea3f8f9729b8883e0aee7d377c1f5e6baa5ccbe81067c1d66@ec2-54-225-94-143.compute-1.amazonaws.com:5432/dclmujdgvt0sbr'
+    SECRET_KEY = 'development mode'
+    SQLALCHEMY_DATABASE_URI = 'postgres://hlugpiywpgwxnx:cb0b209e0de7b05ea3f8f9729b8883e0aee7d377c1f5e6baa5ccbe81067c1d66@ec2-54-225-94-143.compute-1.amazonaws.com:5432/dclmujdgvt0sbr'
 
 class DevelopmentConfig(Config):
     '''
