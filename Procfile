d034a70614cf37e2dc0522a67d2f34a8c8f56a96
Harith Javed Bakhrani <harithjaved@gmail.com>
2017-11-30 19:52:40 +0000

    postgresql db integrated with heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,5 @@
+flask db init
+flask db migrate
+flask db upgrade
 web: python run.py
 heroku ps:scale web=1
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,4 @@
+import os
+
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI =  'postgresql://testuser:abc123@localhost:5432/testdb'
+SQLALCHEMY_DATABASE_URI =  os.getenv('DATABASE_URL', 'postgresql://testuser:abc123@localhost:5432/testdb')
