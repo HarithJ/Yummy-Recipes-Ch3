cbd66b1a6067f13039ef0d1f320cac6c6eb49c34
Harith Javed Bakhrani <harithjaved@gmail.com>
2017-12-15 07:43:25 +0000

    debugging

--- app/api/views.py
+++ app/api/views.py
@@ -279,6 +279,11 @@ def add_or_get_recipe(category_id):
                     recipe_data['id'] = recipe.id
                     recipe_data['title'] = recipe.title
 
+
+                    output.append(recipe_data)
+                    return jsonify({'recipes' : output})
+
+
                     ing_num = 1
                     for ingredient in recipe.recipe_ingredients:
                         recipe_data['ingredient{}'.format(ing_num)] = ingredient
